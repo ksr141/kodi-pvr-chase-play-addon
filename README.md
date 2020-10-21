2bbec3832fb97bb2d0e7cd33060888b6a2a7cd9e
Kaleb Ruof <ksr141@gmail.com>
2020-10-21 10:58:25 -0700

    cleaned up documentation and added setting to control end buffer time

--- README.md
+++ README.md
@@ -1,13 +1,29 @@
-# Welcome to your addon
+## Kodi PVR Chase Play Addon
+This addon enables Kodi to seek while playing an in-progress recording and reaching the end of the video.
 
-1. You might want to move this folder into the kodi addon folder for convinience when debugging. It might also be needed to be `enabled` inside of the kodi addon browser.
-2. Now start coding! Just open up the `.py` file in this folder and create what you would like Kodi to do! If you're creating a plugin, please check out [this kodi routing framework](https://github.com/tamland/kodi-plugin-routing) and copy a version of that module to your kodi addon folder.
-3. Write some tests, maybe? Don't forget to activate [travis](https://travis-ci.org/) access to your repository. We've created a test folder and a travis config file for that, otherwise just delete those ;)
-4. You might want to look at your `addon.xml` it should already be filled, but you will need to understand what your doing and might want to fill in some more info. So read up [here](http://kodi.wiki/view/Addon.xml).
-5. Do you want some settings for your addon? Check the `settings.xml` in the resources folder. And read up [here](http://kodi.wiki/view/Settings.xml).
-6. Read [this info](http://kodi.wiki/view/Add-on_structure#icon.png) and drop an icon for your addon into the `resource` folder and name it `icon.png`.
-7. Read [this](http://kodi.wiki/view/Add-on_structure#fanart.jpg) and drop a addon background into the `resource` folder and name it `fanart.jpg`.
-8. End up with a beautiful Kodi addon! Good for you :) Maybe you want to [share it with us](http://kodi.wiki/view/Submitting_Add-on_updates_on_Github)?
+For example, if you record a program that is an hour long, and begin watching the recording after 15 minutes, Kodi will 
+report the video's length as 15 minutes.  When you hit the 15 minute mark of the recording, Kodi will continue to play it
+but any attempt to seek will bring the video back to the 15 minute mark. I 
+believe this is a limiation with FFMpeg rather than Kodi.
 
-### Debugging
-To get the debug logging to work, just set the global kodi logging to true and the debug logging in your addons settings.
+This addon will catch an attempt to seek past the original duration of the video.
+If the file has grown in size since playback began, the addon will re-start the video file (in order to load the updated
+duration into Kodi) and set the current playing time to the desired seek location.  In the example above, if a seek
+to the 16 minute mark was attempted, the addon will close the video, re-start it and set the current playing time
+to 16 minutes (as long as the recording has reached at least 16 minutes in duration by this time).
+
+### Limitations
+1. Re-starting a video from an addon is not exactly seamless in Kodi.  The
+user will see the video exit, a frame or two from the beginning as it restarts,
+then it should jump to the correct location.
+2. Progressive skip does not work well with this addon.  I recommend using fixed skip intervals.  With progressive
+skip, the Kodi GUI will not let you skip past the video's duration, you can only skip to the end.  The addon should still
+catch this and reload the video, but it's not as intuitive as fixed skip.  Additionally, if Kodi is playing beyond the
+duration of the video (say we're watching at 16 minutes, but Kodi believes the end is at 15 minutes) attempting a progressive
+skip will bring you back to the end of the video (e.g. the progressive skip can only be -1 minute).  The addon attempts
+to catch this and bring you back to where the skip was attempted - the video will be reloaded and the current playing time
+set to 16 minutes.
+3. If a seek is attempted that exceeds the current recording time (e.g the video is reloaded and the requested seek still
+exceeds the recorded video duration) the playing time will be set close to the end of the recorded video.  The default is
+10s from the end, but this can be adjusted through the addon settings.  Note that Kodi behaves erratically with times that
+are very close to the end of the current recording (10s is a pretty safe buffer).
--- resources/lib/chase_player.py
+++ resources/lib/chase_player.py
@@ -1,19 +1,23 @@
 from xbmcvfs import File
+from xbmcaddon import Addon
 import xbmc
 
 class ChasePlayer(xbmc.Player):
 
     def __init__(self):
         xbmc.Player.__init__(self)
-        self.chase_play_event = False
-        self.orig_file_size = 0
-        self.orig_total_time = 0
+        # Addon object to get addon settings
+        self.xbmc_addon = Addon()
+        # initialize class vars
+        self.chase_play_event = False  # flag that a seek occurred past the video's duration
+        self.orig_file_size = 0        # video file size when playback is started
+        self.orig_total_time = 0       # video duration when playback is started
+        self.video_is_file = False     # video is a locally-accessible file
+        self.restart_seek_time = 0     # time to seek
 
     # check for a seek request past the end of the currently playing file.
     # if that file has grown since first starting it, then re-start the video and seek to our requested time
     def onPlayBackSeek(self, time, seek_offset):
-        # check if the desired seek is past the end of the currently playing file, and that the file size has grown
-        # if so, stop and re-start the file to load the new video duration
         # note: after restarting the video and seeking to the initial time, this function will again be called
         #       hence, check if a chase play event (chase_play_event=True) is currently in progress
         if not self.chase_play_event and self.video_is_file and \
@@ -25,28 +29,34 @@ class ChasePlayer(xbmc.Player):
                                                                                         self.orig_file_size,
                                                                                         self.get_playing_file_size()),
                      level=xbmc.LOGNOTICE)
-            self.stop()
-            self.play(self.video_file_name, windowed=False)
+
             # flag that a chase play event occurred
             self.chase_play_event = True
-            # note: if seek attempt occurs past the original duration of the video
+
+            # if seek attempt occurs past the original duration of the video
             # seek_offset will reflect the time elapsed since the end of the video. add this to the restart time
             if seek_offset < 0:
                 self.restart_seek_time = self.orig_total_time-(seek_offset/1000)
             else:
                 self.restart_seek_time = time/1000
 
+            # stop and re-start the video file
+            self.stop()
+            self.play(self.video_file_name, windowed=False)
+
     def onAVStarted(self):
         # check if a seek was attempted past the end of an in-progress recording and triggered a restart of the video
-        # attempt to resume the previously playing video at the same point
+        # attempt to resume the previously playing video at the desired seek location
         if self.chase_play_event:
             xbmc.log("Chase Player Seeking to: %s Total: %s" % (self.restart_seek_time, self.getTotalTime()),
                      level=xbmc.LOGNOTICE)
 
-            # if the new seek location is greater than the video's updated duration
-            # set the current play time to be close (10s) to the end of the video
+            # if the new seek location is still greater than the video's updated duration
+            # set the current play time to be close to the end of the video
             if self.restart_seek_time > self.getTotalTime():
-                self.seekTime(self.getTotalTime()-10)
+                # retrieve our buffer setting
+                end_buffer = int(self.xbmc_addon.getSetting('end_buffer'))
+                self.seekTime(self.getTotalTime()-end_buffer)
             else:
                 self.seekTime(self.restart_seek_time)
 
@@ -57,7 +67,11 @@ class ChasePlayer(xbmc.Player):
             except:
                 self.video_is_file = False
             else:
-                self.video_is_file = True
+                # check that this is a video and not a PVR live-tv URL
+                if self.isPlayingVideo() and not self.video_file_name.startswith("pvr"):
+                    self.video_is_file = True
+                else:
+                    self.video_if_file = False
 
         # clear the chase play event flag
         self.chase_play_event = False
